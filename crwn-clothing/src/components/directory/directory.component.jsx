2dccceea01fe765284a4aed9dfad3b4197d78f0d
amit sharma <amitsharma00261@gmail.com>
2023-04-21 21:25:29 +0530

    Added the category item and directory component

---
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+import CategoryItem from "../category-item/category-item.component"
+import "./directory.styles.scss"
+const Directory = ({categories}) =>{
+      return(
+        <div className="directory-container">         
+        {
+          categories.map(({id, title, imageUrl}) => {
+            return(
+              <CategoryItem id={id} title={title} imageUrl={imageUrl}/>
+            )
+          })
+        }        
+      
+</div>
+      )
+}
+
+export default Directory;
