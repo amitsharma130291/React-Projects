aef096b66695fdde2d386db3f022069ef76fe057
amit sharma <amitsharma00261@gmail.com>
2023-04-26 18:04:55 +0530

    Added the firebase package and signin component

--- crwn-clothing/src/routes/Navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/Navigation/navigation.component.jsx
@@ -8,6 +8,7 @@ const Navigation = () => {
                 <Link to="/" className="logo-container"><CrownLogo /></Link>           
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">Shop</Link>
+                    <Link className="nav-link" to="/sign-in">Sign In</Link>
                 </div>
             </div>
             <Outlet/>           
